739e369f15e98642f7ad3ae9f3483ed71ff1fdd2
RedBeeツ <77657831+bilal-08@users.noreply.github.com>
2022-06-25 10:34:31 -0700

    fix(): added env variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,13 +3,13 @@ import { AppController } from './app.controller.js'
 import { AppService } from './app.service.js'
 import { MongooseModule } from '@nestjs/mongoose'
 import { CodeModule } from './code/code.module.js'
+import { ConfigModule } from '@nestjs/config'
 //imported the CodeModule to be used.
 @Module({
     imports: [
         CodeModule,
-        MongooseModule.forRoot(
-            'mongodb+srv://billa:billasenpai@cluster0.6bhqd.mongodb.net/?retryWrites=true&w=majority',
-        ),
+        ConfigModule.forRoot(),
+        MongooseModule.forRoot(process.env.MONGO_URI),
     ],
     controllers: [AppController],
     providers: [AppService],
